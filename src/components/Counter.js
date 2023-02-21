292f42c7878faf4281a63466de10de4bf9490eb5
iam-MustakAhmedKhan <mustakahmedkhan32@gmail.com>
2023-02-22 00:15:06 +0600

    counter application using useSelector and useDispatch hooks

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { decrement, increment } from '../redux/counter/action';
 
 
-function Counter({count,increment,decrement}) {
+
+function Counter() {
+
+    const count = useSelector(state => state.value)
+    const dispatch = useDispatch()
+    
+    const incrementHandler = () => {
+        dispatch(increment())
+    }
+    const decrementHandler = () => {
+        dispatch(decrement())
+    }
 
     return (
         <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
@@ -11,12 +22,12 @@ function Counter({count,increment,decrement}) {
             <div className="flex space-x-3">
                 <button
                     className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                    onClick={increment}>
+                    onClick={incrementHandler}>
                     Increment
                 </button>
                 <button
                     className="bg-red-400 text-white px-3 py-2 rounded shadow"
-                    onClick={decrement}>
+                    onClick={decrementHandler}>
                     Decrement
                 </button>
             </div>
@@ -24,19 +35,4 @@ function Counter({count,increment,decrement}) {
     );
 }
 
-const mapStateToProps = (state) => {
-    return {
-        count:state.value
-    }
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        increment: () => dispatch(increment()),
-        decrement:()=>dispatch(decrement())
-        
-    }
-}
-
-
-export default connect(mapStateToProps,mapDispatchToProps)(Counter)
+export default Counter
